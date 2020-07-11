69cf44744677396a5ca433d482b4f5b6e8fed0ce
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-07-11 12:38:45 -0500

    section 2B of docs - reactive declarations

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,24 @@
 <script>
   let title = "Reactivity";
   let count = 0;
-  //NEW:
   function handleClick() {
     count += 1;
   }
+  //NEW:
+  $: doubleCount = count * 2;
 </script>
 
 <style>
+  main {
+    width: 1000px;
+    margin: 0 auto;
+  }
+
   h1 {
     color: #ff3e00;
     text-transform: uppercase;
     font-size: 4em;
-    font-weight: 100;
+    font-weight: bold;
     text-align: center;
   }
 
@@ -30,12 +36,31 @@
 <main>
   <h1>{title}</h1>
   <div class="docs-read">
-    At the heart of Svelte is a powerful system of reactivity for keeping the
-    DOM in sync with your application state — for example, in response to an
-    event.
+    <p>
+      Svelte automatically updates the DOM when your component's state changes.
+      Often, some parts of a component's state need to be computed from other
+      parts (such as a fullname derived from a firstname and a lastname), and
+      recomputed whenever they change. For these, we have reactive declarations.
+    </p>
+    <p>
+      When assigning reactive declarations, Svelte interprets the code to mean,
+      <strong>
+        "re-run this code whenever any of the referenced values change"
+      </strong>
+      . This is very similar to the computed property in Vue.
+    </p>
+    <p>
+      Although reactive values are optional, because you can just change the
+      values in the HTML markup, they become particularly valuable when you need
+      to reference them multiple times, or you have values that depend on other
+      reactive values.
+    </p>
   </div>
   <div class="code">
-    <button on:click={handleClick}>Clicked {count} Times</button>
+    <button on:click={handleClick}>
+      Clicked {count} {count === 1 ? 'Time' : 'Times'}
+    </button>
+    <p>{count} doubled is {doubleCount}</p>
   </div>
   <p>
     Visit the
