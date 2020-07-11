76c7214046e94fd99cae0b1a1302d3349da1ec31
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-07-11 11:52:56 -0500

    part 1-introduction. The real coding begins in the next section

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,46 @@
 <script>
-	export let name;
+  export let name;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+  <p>
+    Pour cette première partie, j'écris pas de code. Néanmoins, voici ce qu'il
+    est couvert
+  </p>
+  <ul>
+    <li>Adding Data</li>
+    <li>Dynamic Attributes</li>
+    <li>Styling</li>
+    <li>Nested Components</li>
+    <li>HTML Tags</li>
+    <li>Making an App (Optional to review)</li>
+  </ul>
+</main>
