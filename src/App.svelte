d6629bdbf2b95247870fd6fdcf1c5f53c88c2484
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-08-15 17:06:48 -0500

    keyed each blocks. unique syntaxe. not as straightforward, but stee eezee.

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,22 @@
 <script>
-  let title = "Looping with Each Blocks";
+  import Thing from "./components/Thing.svelte";
 
-  let cats = [
-    { id: "J---aiyznGQ", name: "Keyboard Cat" },
-    { id: "z_AbfPXTKms", name: "Maru" },
-    { id: "OUtn3pvWmpg", name: "Henri The Existential Cat" }
+  let things = [
+    { id: 1, colour: "#0d0887" },
+    { id: 2, colour: "#6a00a8" },
+    { id: 3, colour: "#b12a90" },
+    { id: 4, colour: "#e16462" },
+    { id: 5, colour: "#fca636" }
   ];
+
+  function handleClick() {
+    things = things.slice(1);
+  }
+
+  let title = "Keyed Each Blocks";
+
+  //TESTING:
+  $: console.log(things);
 </script>
 
 <style>
@@ -37,31 +48,30 @@
   <h1>{title}</h1>
   <div class="docs-read">
     <p>
-      If you need to loop over lists of data, use an
-      <strong>each block</strong>
-      . You can get the current index as a second argument, like so
+      Just like in Vue and React, it is recommended to insert a unique key into
+      our iterations. Not doing so won't break the app, but could lead to
+      unexpected behaviours. Especially when manipulating data.
+    </p>
+    <p>
+      <strong>
+        We make a key by putting the value of the key in parantheses in our each
+        block.
+      </strong>
     </p>
   </div>
 
   <div class="code">
-    <ul>
-      {#each cats as currentCat, index}
-        <li>
-          <a
-            href="https://www.youtube.com/watch?v={currentCat.id}"
-            target="_blank">
-            {index + 1}: {currentCat.name}
-          </a>
-        </li>
-      {/each}
-    </ul>
+    <button on:click={handleClick}>Remove first thing</button>
+    {#each things as currentThing (currentThing.id)}
+      <Thing current={currentThing.colour} />
+    {/each}
   </div>
 
-  <!-- small NOTE: not relevant to the topic. But interesting how we don't have to use string interpolation or template literals, or even binding for the href attribute above. We just our brackets as usual and our data, and it works. Maybe I'm missing something.. Either way, keep this in mind. -->
+  <!-- Try removing them by removing the key declaraion. The behaviour is bizzare. Je peux pas l'expliquer. -->
 
   <p>
     S'il te faut refraîchir la tête au sujet, consulter ce lien:
-    <a href="https://svelte.dev/tutorial/else-if-blocks" target="_blank">
+    <a href="https://svelte.dev/tutorial/keyed-each-blocks" target="_blank">
       Svelte tutorial
     </a>
   </p>
